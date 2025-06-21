3d3ce864c35f47ecfdf323054c387b71e7050355
Ekin <1197026828@qq.com>
2025-06-21 15:44:33 +1000

    achieve the functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const openBtn = document.querySelector('.open');
+const closeBtn = document.querySelector('.close');
+const navMenu = document.querySelector('.menu');
+const overLay = document.querySelector('.overlay');
+
+openBtn.addEventListener('click', () => {
+  navMenu.classList.add('active');
+  overLay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+  overLay.classList.remove('active');
+});
+
+overLay.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    overLay.classList.remove('active');
+});
